3e450fc5d7a1f4ba973b9a66fe39fd0f8da69f09
breastcover <39230048+breastcover@users.noreply.github.com>
2018-10-15 16:27:19 +0800

    day sixtyeight one

---
+++ leetcode-709.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(char& c:str)
+        {
+            if(c>='A'&&c<='Z')
+                c+=32;
+        }
+        return str;
+    }
+};class Solution {
+public:
+    string toLowerCase(string str) {
+        for(char& c:str)
+        {
+            if(c>='A'&&c<='Z')
+                c+=32;
+        }
+        return str;
+    }
+};
